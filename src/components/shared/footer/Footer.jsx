5574c1a008c5aa85c8482887fb21c90d05112f4c
alet89 <alessandrotoschi@me.com>
2021-02-07 15:42:07 +0100

    ttt

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
 
         <div className="Footer">
-            <p>SFeeqr re gqer qgw rrwq  rwqg rgqweg</p>
+            <p>Covenats is a protocol by <a href="https://ethereansos.eth.link">EthOS</a>. This is an R&D project <b>use it at your own risk!</b> This protocol is ruled by the <a href="https://dapp.dfohub.com/?addr=0xeFAa6370A2ebdC47B12DBfB5a07F91A3182B5684">Covenants DFO</a> A Fully On-Chain Organization, without any real world legal entity involved. If you find a bug, please help us to improve by our <a>Github</a></p>
             <img src={city}></img>
         </div>
     )
