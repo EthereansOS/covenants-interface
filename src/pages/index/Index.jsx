91632710a30238f412010f9597c025fe180b36a8
alet89 <alessandrotoschi@me.com>
2021-02-08 17:46:18 +0100

    index detumorata

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,18 +1,22 @@
 import dragonImage from '../../assets/images/index.png';
 
-const lorem = 'Here you will find Covenants. Empowered by The Aggregator, these contracts allow anyone to create, control and customize their own DeFi experience.';
+const lorem = 'Whether you are an ape, a penguin or a wizard, you can call this place home. We offer a haven for all Ethereans seeking freedom and prosperity';
+const lorem2 = 'Here you will find Covenants. Empowered by The Aggregator, these contracts allow anyone to create, control and customize their own DeFi experience like never before.';
 
 const Index = () => {
     return (
         <div className="page-container-wrapper">
             <div className="page-container">
                 <div className="page-container-row index-container-row">
-                    <div className="page-container-col index-container-col">
-                        <img src={dragonImage} className="index-image" />
-                        <h2 className="index-title">Covenants</h2>
+                    <div className="page-container-col index-container-col CovTITLE">
+                        <figure>
+                            <img src={dragonImage} className="index-image" />
+                        </figure>
                     </div>
-                    <div className="page-container-col">
+                    <div className="page-container-col DescCov">
                         <p className="index-paragraph">{lorem}</p>
+                        <h5 className="welcome">Welcome</h5>
+                        <p className="index-paragraph">{lorem2}</p>
                     </div>
                 </div>
             </div>
