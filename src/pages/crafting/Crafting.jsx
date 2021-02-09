71e2716f8aa610433101774c36ca493d3c806b1b
alet89 <alessandrotoschi@me.com>
2021-02-09 03:30:16 +0100

    Stats Decostrut and recovered

--- src/pages/crafting/Crafting.jsx
+++ src/pages/crafting/Crafting.jsx
@@ -3,7 +3,7 @@ import dragonImage from '../../assets/images/4.png';
 import { Route, Switch } from "react-router-dom";
 import CraftingDapp from './dapp/dapp';
 
-const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque quia consequuntur id magnam tempore animi eveniet quod dolor ipsa illo? Iure illo alias rem sapiente illum error laborum voluptatum quasi!';
+const lorem = 'Covenant wizards have made a breakthrough in the science of liquidity. Ethereans can now Craft it at the molecular level, allowing for more deeply programmable pools than ever thought possible.\nAlready, a crack team of entrepreneurial penguins have innovated Craftable Initial Liquidity Offerings (ILOs). Projects can use these to provide initial liquidity for tokens, secure it from Sniper bots and more.\nThe Wizard scientists will be publishing their latest findings soon. In the meantime, you can read their first report here ';
 
 const Crafting = () => {
     return (
