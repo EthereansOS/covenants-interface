458d74744ee7020b5c205e1fb0095291646717ff
Paolo Rollo <paolo.rollo1997@gmail.com>
2021-02-19 10:14:15 +0100

    update.

--- src/components/shared/bazaar/BazaarComponent.jsx
+++ src/components/shared/bazaar/BazaarComponent.jsx
@@ -7,81 +7,6 @@ import axios from 'axios';
 
 const BazaarComponent = (props) => {
     const { hasBorder, className, indexToken } = props;
-    const [loading, setLoading] = useState(true);
-    const [metadata, setMetadata] = useState({});
-
-    useEffect(() => {
-        getData();
-    }, []);
-
-    const getData = async () => {
-        setLoading(true);
-        try {
-            const indexContract = await props.dfoCore.getContract(props.dfoCore.getContextElement('IndexABI'), props.dfoCore.getContextElement('indexAddress'));
-            const interoperableContract = await props.dfoCore.getContract(props.dfoCore.getContextElement('IEthItemInteroperableInterfaceABI'), indexToken.address);
-            const objectId = await interoperableContract.methods.objectId().call();
-            const mainInterface = await interoperableContract.methods.mainInterface().call();
-            const contract = await props.dfoCore.getContract(props.dfoCore.getContextElement('IEthItemABI'), mainInterface);
-            const info = await indexContract.methods.info(objectId, 0).call();
-            const uri = await contract.methods.uri(objectId).call();
-            let res = { data: { description: '', image: '' } };
-            try {
-                res = await axios.get(uri);
-            } catch (error) {
-                console.log('error while reading metadata from ipfs..')
-            }
-            let total = 0;
-            await Promise.all(info._amounts.map(async (amount, index) => {
-                try {
-                    const token = info._tokens[index];
-                    const tokenContract = await props.dfoCore.getContract(props.dfoCore.getContextElement('ERC20ABI'), token);
-                    const decimal = await tokenContract.methods.decimals().call();
-                    const res = await axios.get(props.dfoCore.getContextElement('coingeckoCoinPriceURL') + token);
-                    const { data } = res;
-                    const tokenPrice = data[token.toLowerCase()].usd;
-                    const value = parseFloat(props.dfoCore.toDecimals(amount, decimal)) * tokenPrice;
-                    total += value;
-                } catch (error) {
-                    console.error(error);
-                }
-            }))
-            const percentages = {};
-            await Promise.all(info._tokens.map(async (token, index) => {
-                try {
-                    const amount = info._amounts[index];
-                    const tokenContract = await props.dfoCore.getContract(props.dfoCore.getContextElement('ERC20ABI'), token);
-                    const decimal = await tokenContract.methods.decimals().call();
-                    const amountDecimals = props.dfoCore.toDecimals(amount.toString(), decimal);
-                    try {
-                        const res = await axios.get(props.dfoCore.getContextElement('coingeckoCoinPriceURL') + token);
-                        const { data } = res;
-                        const tokenPrice = data[token.toLowerCase()].usd;
-                        percentages[token] = ((parseFloat(amountDecimals) * tokenPrice) / parseInt(total)) * 100;
-                    } catch (error) {
-                        console.error(error);
-                        percentages[token] = 0;
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            }));
-            setMetadata({ percentages });
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    if (loading) {
-        return (<div className={className}>
-            <div className="col-12 justify-content-center">
-                <div className="spinner-border text-secondary" role="status">
-                    <span className="visually-hidden"></span>
-                </div>
-            </div>
-        </div>)
-    }
 
     return (
         <div className={className}>
@@ -96,7 +21,7 @@ const BazaarComponent = (props) => {
                                 <p> {
                                     indexToken.info._tokens.map((token, index) => {
                                         return (
-                                            <><p>{window.formatMoney(metadata.percentages[token], 0)}% <Coin address={token} /> { index !== indexToken.info._tokens.length - 1 && " " }</p></>
+                                            <><p>{indexToken.amounts[token]} <Coin address={token} /> { index !== indexToken.info._tokens.length - 1 && " " }</p></>
                                         )
                                     })
                                 }</p>
