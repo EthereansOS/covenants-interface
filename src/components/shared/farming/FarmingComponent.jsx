fe344496fc6ced3f710fffd1b51dd5681d25b15c
alet89 <alessandrotoschi@me.com>
2021-04-19 21:22:10 +0200

    sss

--- src/components/shared/farming/FarmingComponent.jsx
+++ src/components/shared/farming/FarmingComponent.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const FarmingComponent = (props) => {
     const { className, goBack, metadata, dfoCore, withoutBack } = props;
     const symbol = metadata.name.replace("Farm ", "");
+    const symbol = metadata.name.replace("Farm ", "");
 
     return (
         <div className={className}>
@@ -14,21 +15,19 @@ const FarmingComponent = (props) => {
                         { metadata.rewardTokenAddress !== dfoCore.voidEthereumAddress ? <a target="_blank" href={`${props.dfoCore.getContextElement("etherscanURL")}token/${metadata.rewardTokenAddress}`} ><Coin height={45} address={metadata.rewardTokenAddress} /></a> : <Coin height={45} address={metadata.rewardTokenAddress} />}
                     </figure>
                     <aside>
-                        <h6><b>{metadata.name}</b> {(metadata.freeSetups.length + metadata.lockedSetups.length === 0 && !metadata.canActivateSetup) ? <span className="text-danger"><b>(inactive)</b></span> : <></> }</h6>
+                        <h6>{window.dfoCore.isItemSync(metadata.rewardTokenAddress) && <b> {metadata.contractAddress}</b>} {!window.dfoCore.isItemSync(metadata.rewardTokenAddress) && <b> {metadata.name}</b>} {(metadata.freeSetups.length + metadata.lockedSetups.length === 0 && !metadata.canActivateSetup) ? <span className="text-danger"><b>(inactive)</b></span> : <></> }</h6>
                         { !withoutBack && <Link to={ goBack ? `/farm/dapp/` : `/farm/dapp/${metadata.contractAddress}`} className={ goBack ? "backActionBTN" : "web2ActionBTN" }>{ goBack ? "Back" : "Open" }</Link>}
                     </aside>
                 </div>
                 <div className="FarmThings">
                         <p><b>Daily Rate</b>: {metadata.rewardPerBlock * 6400} {symbol}</p>
-                        {/*<p><b>APY</b>: 20%</p> If 0 (no coingecko Info) insert "Not Available"*/}
                         <p><b>Active Setups</b>: {metadata.freeSetups.length + metadata.lockedSetups.length} </p>
                         <div className="StatsLink">
+                            {window.dfoCore.isItemSync(metadata.rewardTokenAddress) && <a className="specialITEMlink" target="_blank" href={props.dfoCore.getContextElement("itemURLTemplate").format(metadata.rewardTokenAddress)}> ITEM</a> }
                             <a target="_blank" href={`${props.dfoCore.getContextElement("etherscanURL")}address/${metadata.contractAddress}`}>Contract</a>
                             <a target="_blank" href={`${props.dfoCore.getContextElement("etherscanURL")}address/${metadata.fullhost}`}>Host</a>
                             <a target="_blank" href={`${props.dfoCore.getContextElement("etherscanURL")}address/${metadata.fullExtension}`}>Extension</a>
                         </div>
-                        {/*(Deprecated)<p><b>Setups</b>: {metadata.totalFreeSetups} free | {metadata.totalLockedSetups} locked</p>*/}
-                        {/*<p><b>Host</b>: <a target="_blank" href={"https://etherscan.io/address/" + metadata.fullhost}>{metadata.host}</a></p>*/}
                 </div>
                 {goBack && <>
                     <div className="FarmThings">
